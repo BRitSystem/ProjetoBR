dec54eb39853ad1954c49e8bb1f1095b8365a541
Rafael Junqueira <rafael.junqueira@futurum.com.br>
2020-07-12 20:56:13 -0300

    ajustes criação pagina

---
+++ WebAdmin/Controllers/InstitucionalController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAdmin.Controllers
+{
+    public class InstitucionalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update(string titulo, string descricao, string RTEConteudo)
+        {
+            ViewBag.Name = string.Format("Name: {0} {1}", titulo, descricao, RTEConteudo);
+            return View();
+        }
+    }
+}
